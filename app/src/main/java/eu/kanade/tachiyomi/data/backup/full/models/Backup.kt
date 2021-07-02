66a376a3a40c0f48b6f9e4907dedf0204a1ac971
jmir1 <jhmiramon@gmail.com>
2021-07-02 23:44:39 +0200

    fix restoring backups from tachiyomi (that don't...
    have any anime)

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/full/models/Backup.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/full/models/Backup.kt
@@ -5,9 +5,9 @@ import kotlinx.serialization.protobuf.ProtoNumber
 
 @Serializable
 data class Backup(
-    @ProtoNumber(1) val backupManga: List<BackupManga>,
+    @ProtoNumber(1) val backupManga: List<BackupManga> = emptyList(),
     @ProtoNumber(2) var backupCategories: List<BackupCategory> = emptyList(),
-    @ProtoNumber(3) val backupAnime: List<BackupAnime>,
+    @ProtoNumber(3) val backupAnime: List<BackupAnime> = emptyList(),
     @ProtoNumber(4) var backupCategoriesAnime: List<BackupCategory> = emptyList(),
     // Bump by 100 to specify this is a 0.x value
     @ProtoNumber(100) var backupSources: List<BackupSource> = emptyList(),
