82c944361486e3cb1bdc8e678784034c1c5b1157
jmir1 <jhmiramon@gmail.com>
2022-03-20 18:59:12 +0100

    add disgusting play button (need to change)

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerActivity.kt
@@ -523,7 +523,7 @@ class PlayerActivity :
     }
 
     private fun updatePlaybackStatus(paused: Boolean) {
-        val r = if (paused) R.drawable.ic_play_arrow_black_24dp else R.drawable.ic_pause_black_24dp
+        val r = if (paused) R.drawable.ic_play_arrow_100dp else R.drawable.ic_pause_100dp
         binding.playBtn.setImageResource(r)
 
         if (paused) {
