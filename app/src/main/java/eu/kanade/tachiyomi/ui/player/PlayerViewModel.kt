2beac61e2c3ce5fdc6eb74bbd72bfcbbbcbe727d
Quickdesh <devesh.ratra@gmail.com>
2023-02-27 04:53:57 -0500

    Update PlayerViewModel.kt

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerViewModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerViewModel.kt
@@ -86,7 +86,7 @@ class PlayerViewModel(
     private val downloadPreferences: DownloadPreferences = Injekt.get(),
     private val playerPreferences: PlayerPreferences = Injekt.get(),
     private val trackPreferences: TrackPreferences = Injekt.get(),
-    private val delayedTrackingStore: DelayedTrackingStore = Injekt.get(),
+    private val delayedTrackingStore: DelayedAnimeTrackingStore = Injekt.get(),
     private val getAnime: GetAnime = Injekt.get(),
     private val getNextEpisodes: GetNextEpisodes = Injekt.get(),
     private val getEpisodeByAnimeId: GetEpisodeByAnimeId = Injekt.get(),
@@ -126,7 +126,7 @@ class PlayerViewModel(
 
     private var currentVideoList: List<Video>? = null
 
-    var requestedSecond: Long = 0L
+    private var requestedSecond: Long = 0L
 
     /**
      * Episode list for the active anime. It's retrieved lazily and should be accessed for the first
@@ -301,6 +301,7 @@ class PlayerViewModel(
      * seen, update tracking services, enqueue downloaded episode deletion and download next episode.
      */
     fun onSecondReached(position: Int, duration: Int) {
+        if (state.value.isLoadingAdjacentEpisode) return
         val currentEpisode = currentEpisode ?: return
         if (episodeId == -1L) return
 
@@ -457,10 +458,8 @@ class PlayerViewModel(
                         location = Location.Pictures.create(relativePath),
                     ),
                 )
-                launchUI {
-                    notifier.onComplete(uri)
-                    eventChannel.send(Event.SavedImage(SaveImageResult.Success(uri)))
-                }
+                notifier.onComplete(uri)
+                eventChannel.send(Event.SavedImage(SaveImageResult.Success(uri)))
             } catch (e: Throwable) {
                 notifier.onError(e.message)
                 eventChannel.send(Event.SavedImage(SaveImageResult.Error(e)))
@@ -563,7 +562,7 @@ class PlayerViewModel(
                                     insertTrack.await(updatedTrack)
                                 } else {
                                     delayedTrackingStore.addAnimeItem(updatedTrack)
-                                    DelayedTrackingUpdateJob.setupTask(context)
+                                    DelayedAnimeTrackingUpdateJob.setupTask(context)
                                 }
                             }
                         }
@@ -618,11 +617,16 @@ class PlayerViewModel(
      */
     fun setAnimeSkipIntroLength(skipIntroLength: Int) {
         val anime = anime ?: return
-        runBlocking {
+        viewModelScope.launchIO {
             setAnimeViewerFlags.awaitSetSkipIntroLength(anime.id, skipIntroLength.toLong())
+            logcat(LogPriority.INFO) { "New Skip Intro Length is ${anime.skipIntroLength}" }
+            mutableState.update {
+                it.copy(
+                    anime = getAnime.await(anime.id),
+                )
+            }
+            eventChannel.send(Event.SetAnimeSkipIntro(getAnimeSkipIntroLength()))
         }
-
-        logcat(LogPriority.INFO) { "Skip Intro Length is ${anime.skipIntroLength}" }
     }
 
     /**
