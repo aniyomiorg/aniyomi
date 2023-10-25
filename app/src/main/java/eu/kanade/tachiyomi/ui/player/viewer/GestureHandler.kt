ee33c145a52890a15c12b102e54c33003efb6b35
Secozzi <49240133+Secozzi@users.noreply.github.com>
2023-10-25 08:55:58 +0000

    fix(player): Don't open screenshot sheet when player is locked (#1172)

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/viewer/GestureHandler.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/viewer/GestureHandler.kt
@@ -113,6 +113,7 @@ class GestureHandler(
     }
 
     override fun onLongPress(e: MotionEvent) {
+        if (SeekState.mode == SeekState.LOCKED) { playerControls.toggleControls(); return }
         activity.openScreenshotSheet()
     }
 }
