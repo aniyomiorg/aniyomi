456916938a395913bfee310d499c28e11f0f56ca
Quickdesh <devesh.ratra@gmail.com>
2023-11-02 16:58:51 -0400

    base cast commit

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/viewer/PlayerControlsView.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/viewer/PlayerControlsView.kt
@@ -13,6 +13,7 @@ import android.view.animation.AnimationUtils
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import com.google.android.gms.cast.framework.CastButtonFactory
 import eu.kanade.tachiyomi.R
 import eu.kanade.tachiyomi.databinding.PlayerControlsBinding
 import eu.kanade.tachiyomi.ui.player.PlayerActivity
@@ -153,6 +154,8 @@ class PlayerControlsView @JvmOverloads constructor(context: Context, attrs: Attr
         binding.titleSecondaryTxt.setOnClickListener { activity.viewModel.showEpisodeList() }
 
         binding.episodeListBtn.setOnClickListener { activity.viewModel.showEpisodeList() }
+
+        CastButtonFactory.setUpMediaRouteButton(activity.applicationContext, binding.castBtn)
     }
 
     private fun switchEpisode(previous: Boolean) {
@@ -410,6 +413,13 @@ class PlayerControlsView @JvmOverloads constructor(context: Context, attrs: Attr
             player.paused!! -> animationHandler.removeCallbacks(fadeOutControlsRunnable)
             binding.unlockedView.isVisible -> showAndFadeControls()
         }
+
+        if (player.paused == true) {
+            activity.mCastSession!!.remoteMediaClient?.pause()
+        } else {
+            activity.mCastSession!!.remoteMediaClient?.play()
+        }
+
     }
 
     // Fade out Player information text
