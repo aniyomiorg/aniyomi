2b8000b81c61a53b13bd4405c18d3afa0be9a537
jmir1 <jhmiramon@gmail.com>
2023-11-14 23:46:11 +0100

    chore: Bump version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "xyz.jmir.tachiyomi.mi"
 
-        versionCode = 105
+        versionCode = 106
         versionName = "0.14.6"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
