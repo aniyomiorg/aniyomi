4baf786d57fdbd08bf46ec5d2fed7ad7e6ae8875
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-11-26 13:43:13 +0100

    use formatTime function

--- app/src/main/java/eu/kanade/presentation/entries/anime/AnimeScreen.kt
+++ app/src/main/java/eu/kanade/presentation/entries/anime/AnimeScreen.kt
@@ -936,8 +936,8 @@ private fun LazyListScope.sharedEpisodeItems(
                         ?.let {
                             stringResource(
                                 R.string.episode_progress,
-                                formatProgress(it),
-                                formatProgress(item.episode.totalSeconds),
+                                formatTime(it),
+                                formatTime(item.episode.totalSeconds),
                             )
                         },
                     scanlator = item.episode.scanlator.takeIf { !it.isNullOrBlank() },
@@ -972,26 +972,6 @@ private fun LazyListScope.sharedEpisodeItems(
                 )
             }
         }
-    }
-}
-
-private fun formatProgress(milliseconds: Long): String {
-    return if (milliseconds > 3600000L) {
-        String.format(
-            "%d:%02d:%02d",
-            TimeUnit.MILLISECONDS.toHours(milliseconds),
-            TimeUnit.MILLISECONDS.toMinutes(milliseconds) -
-                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(milliseconds)),
-            TimeUnit.MILLISECONDS.toSeconds(milliseconds) -
-                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliseconds)),
-        )
-    } else {
-        String.format(
-            "%d:%02d",
-            TimeUnit.MILLISECONDS.toMinutes(milliseconds),
-            TimeUnit.MILLISECONDS.toSeconds(milliseconds) -
-                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(milliseconds)),
-        )
     }
 }
 
