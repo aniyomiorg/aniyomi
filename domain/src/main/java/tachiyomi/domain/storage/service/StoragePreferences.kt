8f50ddfb11ec9fd6ecfcca397c9ff7c4c9485f01
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-11-28 18:42:21 +0100

    rename manga download val

--- domain/src/main/java/tachiyomi/domain/storage/service/StoragePreferences.kt
+++ domain/src/main/java/tachiyomi/domain/storage/service/StoragePreferences.kt
@@ -12,7 +12,7 @@ class StoragePreferences(
 
     companion object {
         const val BACKUP_DIR = "backup"
-        const val DOWNLOADS_DIR = "manga_downloads"
+        const val DOWNLOADS_MANGA_DIR = "manga_downloads"
         const val DOWNLOADS_ANIME_DIR = "anime_downloads"
     }
 }
