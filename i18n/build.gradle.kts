2e096b64b441c73f7a8a3426e9b1855c1bc8096e
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2023-12-02 17:09:45 +0100

    prep

--- i18n/build.gradle.kts
+++ i18n/build.gradle.kts
@@ -1,14 +1,39 @@
 plugins {
+    kotlin("multiplatform")
     id("com.android.library")
-    kotlin("android")
+    id("dev.icerock.mobile.multiplatform-resources")
+}
+
+kotlin {
+    androidTarget()
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                api(libs.moko.core)
+            }
+        }
+        val androidMain by getting {
+            dependsOn(commonMain) // https://github.com/icerockdev/moko-resources/issues/562
+        }
+    }
 }
 
 android {
     namespace = "tachiyomi.i18n"
 
+    sourceSets {
+        named("main") {
+            res.srcDirs("src/commonMain/resources", "src/aniyomiMain/resources")
+        }
+    }
+
     lint {
         disable.addAll(listOf("MissingTranslation", "ExtraTranslation"))
     }
+}
+
+multiplatformResources {
+    multiplatformResourcesPackage = "tachiyomi.i18n"
 }
 
 tasks {
@@ -22,7 +47,7 @@ tasks {
     }
 
     preBuild {
-        dependsOn(copyHebrewStrings, localesConfigTask)
+        dependsOn(localesConfigTask)
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.freeCompilerArgs += listOf(
