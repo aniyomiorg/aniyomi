4405541b2fcffd61ee90e9bd945de23025da911b
Claudemirovsky <63046606+Claudemirovsky@users.noreply.github.com>
2023-12-21 06:49:05 -0300

    fix(app): Don't ignore the anime side in SEARCH/SEND intents

--- app/src/main/java/eu/kanade/tachiyomi/ui/deeplink/manga/DeepLinkMangaActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/deeplink/manga/DeepLinkMangaActivity.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.ui.deeplink.manga
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import eu.kanade.tachiyomi.ui.deeplink.DeepLinkScreenType
 import eu.kanade.tachiyomi.ui.main.MainActivity
 
 class DeepLinkMangaActivity : Activity() {
@@ -12,6 +13,7 @@ class DeepLinkMangaActivity : Activity() {
 
         intent.apply {
             flags = flags or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+            putExtra(MainActivity.INTENT_SEARCH_TYPE, DeepLinkScreenType.MANGA.toString())
             setClass(applicationContext, MainActivity::class.java)
         }
         startActivity(intent)
