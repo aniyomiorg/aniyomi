cfc98adc6a388bd134fc0ff9a470a3280c2acdd1
giorgionegro <43270431+giorgionegro@users.noreply.github.com>
2023-12-31 16:18:23 +0100

    Update DownloadPreferences.kt

--- domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
+++ domain/src/main/java/tachiyomi/domain/download/service/DownloadPreferences.kt
@@ -65,7 +65,7 @@ class DownloadPreferences(
     )
 
     fun numberOfDownloads() = preferenceStore.getInt("download_slots", 1)
-    fun multithreadingDownload() = preferenceStore.getBoolean("multi_part_download", true)
+    fun multithreadingDownload() = preferenceStore.getBoolean("multi_part_download", false)
 
-    fun numberOfThreads() = preferenceStore.getInt("download_threads", 32)
+    fun numberOfThreads() = preferenceStore.getInt("download_threads", 4)
 }
