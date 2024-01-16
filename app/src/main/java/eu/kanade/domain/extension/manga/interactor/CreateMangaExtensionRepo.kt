6b0bbdc8a9e2db51b733f33572ad7b59a83b31d1
jmir1 <jhmiramon@gmail.com>
2024-01-16 14:54:54 +0100

    fix: Repo URL must end with json

--- app/src/main/java/eu/kanade/domain/extension/manga/interactor/CreateMangaExtensionRepo.kt
+++ app/src/main/java/eu/kanade/domain/extension/manga/interactor/CreateMangaExtensionRepo.kt
@@ -35,4 +35,4 @@ class CreateMangaExtensionRepo(private val preferences: SourcePreferences) {
 
 const val OFFICIAL_TACHIYOMI_REPO_BASE_URL = "https://raw.githubusercontent.com/tachiyomiorg/extensions/repo"
 private val repoRegex = """^https://.*/index\.min\.json$""".toRegex()
-private val githubRepoRegex = """https://github\.com/[^/]+/[^/]+/blob/(?:[^/]+/)+index\.min\.json""".toRegex()
+private val githubRepoRegex = """https://github\.com/[^/]+/[^/]+/blob/(?:[^/]+/)+index\.min\.json$""".toRegex()
