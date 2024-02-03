d52f07265b2490ff6e3e73b62cc2b876a8acac91
LuftVerbot <97435834+LuftVerbot@users.noreply.github.com>
2024-02-03 12:22:04 +0100

    Make v0.3 ready

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         applicationId = "xyz.luft.tachiyomi.mi"
 
         versionCode = 121
-        versionName = "0.2.2"
+        versionName = "0.3"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
