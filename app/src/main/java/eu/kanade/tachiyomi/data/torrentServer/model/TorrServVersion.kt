c0c52791a4178ab35b6b003b5f4cbd635e4a9ad0
DiegoPYL1209 <diegopyl1209@gmail.com>
2024-04-28 19:44:24 -0400

    delete unused stuff

--- app/src/main/java/eu/kanade/tachiyomi/data/torrentServer/model/TorrServVersion.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.data.torrentServer.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class TorrServVersion(
-    val version: String,
-    val links: Map<String, String>,
-)
