36113c7f6cf6d689595788972225116ffd434b66
Secozzi <folke.steen85@gmail.com>
2024-05-08 17:16:04 +0200

    Install build-tools 29.0.3
    Co-authored-by: arkon <4098258+arkon@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,6 @@ val SUPPORTED_ABIS = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
 
 android {
     namespace = "eu.kanade.tachiyomi"
-    buildToolsVersion = "34.0.0"
 
     defaultConfig {
         applicationId = "xyz.jmir.tachiyomi.mi"
