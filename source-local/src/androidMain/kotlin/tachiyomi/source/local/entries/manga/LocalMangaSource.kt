74c66fe49dabd572d3c5340e6972cceba4a8063e
Secozzi <folke.steen85@gmail.com>
2024-07-01 14:55:50 +0200

    Address `ZipFile` deprecation warning
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- source-local/src/androidMain/kotlin/tachiyomi/source/local/entries/manga/LocalMangaSource.kt
+++ source-local/src/androidMain/kotlin/tachiyomi/source/local/entries/manga/LocalMangaSource.kt
@@ -17,9 +17,9 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
 import logcat.LogPriority
+import mihon.core.common.extensions.toZipFile
 import nl.adaptivity.xmlutil.core.AndroidXmlReader
 import nl.adaptivity.xmlutil.serialization.XML
-import org.apache.commons.compress.archivers.zip.ZipFile
 import tachiyomi.core.common.i18n.stringResource
 import tachiyomi.core.common.storage.extension
 import tachiyomi.core.common.storage.nameWithoutExtension
@@ -217,7 +217,7 @@ actual class LocalMangaSource(
         for (chapter in chapterArchives) {
             when (Format.valueOf(chapter)) {
                 is Format.Zip -> {
-                    ZipFile(chapter.openReadOnlyChannel(context)).use { zip: ZipFile ->
+                    chapter.openReadOnlyChannel(context).toZipFile().use { zip ->
                         zip.getEntry(COMIC_INFO_FILE)?.let { comicInfoFile ->
                             zip.getInputStream(comicInfoFile).buffered().use { stream ->
                                 return copyComicInfoFile(stream, folder)
@@ -365,7 +365,7 @@ actual class LocalMangaSource(
                     entry?.let { coverManager.update(manga, it.openInputStream()) }
                 }
                 is Format.Zip -> {
-                    ZipFile(format.file.openReadOnlyChannel(context)).use { zip ->
+                    format.file.openReadOnlyChannel(context).toZipFile().use { zip ->
                         val entry = zip.entries.toList()
                             .sortedWith { f1, f2 ->
                                 f1.name.compareToCaseInsensitiveNaturalOrder(
