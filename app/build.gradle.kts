f4f6986cacb8cd5bd887eadf5290772e714627ef
Secozzi <folke.steen85@gmail.com>
2024-07-03 19:01:00 +0200

    Migrated from Accompanist Webview to KevinZou WebView
    * Migrated from Accompanist Webview to KevinZou WebView to preempt deprecation
    
    * Removed old webview from version library
    
    Co-authored-by: Maddie Witman <1882979+sirlag@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -172,7 +172,6 @@ dependencies {
     debugImplementation(compose.ui.tooling)
     implementation(compose.ui.tooling.preview)
     implementation(compose.ui.util)
-    implementation(compose.accompanist.webview)
     implementation(compose.accompanist.systemuicontroller)
 
     implementation(androidx.paging.runtime)
@@ -249,6 +248,8 @@ dependencies {
     implementation(libs.bundles.voyager)
     implementation(libs.compose.materialmotion)
     implementation(libs.swipe)
+    implementation(libs.compose.webview)
+
 
     // Logging
     implementation(libs.logcat)
