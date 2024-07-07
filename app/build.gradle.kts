b5394ae9a61e90db3eaff56b29ddb6b7ba65aefd
Dark25 <nadiecaca2000@gmail.com>
2024-07-07 22:03:18 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         applicationId = "com.dark.animetailv2"
 
         versionCode = 124
-        versionName = "0.16.4.5"
+        versionName = "0.16.4.6"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
