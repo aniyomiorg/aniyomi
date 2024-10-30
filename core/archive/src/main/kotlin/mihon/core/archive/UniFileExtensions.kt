8ce93b5402f1345dd3c6937c7bf8df97ea41c39c
Secozzi <folke.steen85@gmail.com>
2024-10-30 12:01:58 +0100

    `spotlessApply` my beloved

--- core/archive/src/main/kotlin/mihon/core/archive/UniFileExtensions.kt
+++ core/archive/src/main/kotlin/mihon/core/archive/UniFileExtensions.kt
@@ -5,7 +5,8 @@ import android.os.ParcelFileDescriptor
 import com.hippo.unifile.UniFile
 
 fun UniFile.openFileDescriptor(context: Context, mode: String): ParcelFileDescriptor =
-    context.contentResolver.openFileDescriptor(uri, mode) ?: error("Failed to open file descriptor: ${filePath ?: uri.toString()}")
+    context.contentResolver.openFileDescriptor(uri, mode)
+        ?: error("Failed to open file descriptor: ${filePath ?: uri.toString()}")
 
 fun UniFile.archiveReader(context: Context) = openFileDescriptor(context, "r").use { ArchiveReader(it) }
 
