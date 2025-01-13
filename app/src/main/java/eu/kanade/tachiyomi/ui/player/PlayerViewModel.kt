afd4f1a22839f38915ca5c67cc33eeb8b0d10139
pero <81130286+perokhe@users.noreply.github.com>
2025-01-13 08:07:11 -0600

    Fix episode being marked as seen at start

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerViewModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerViewModel.kt
@@ -1009,6 +1009,7 @@ class PlayerViewModel @JvmOverloads constructor(
         if (isLoadingEpisode.value) return
         val currentEp = currentEpisode.value ?: return
         if (episodeId == -1L) return
+        if (duration == 0) return
 
         val seconds = position * 1000L
         val totalSeconds = duration * 1000L
