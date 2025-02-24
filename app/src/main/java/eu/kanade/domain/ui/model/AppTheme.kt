161518e2dd2393e04ce4c8e889fa5594ca096538
MajorTanya <39014446+majortanya@users.noreply.github.com>
2025-02-25 00:22:48 +0100

    Add "Monochrome" theme
    This theme is mainly geared towards e-Ink displays with limited/no
    colour capabilities. Previous themes like Yin & Yang would make heavy
    use of greyscale colours which could look off on some devices.
    
    This theme is probably not conformant to Material Design 3 colour
    scheme guidelines, but it does boast some amazing WebAIM contrast
    ratios (#FFFFFF text on #000000 background gets a ratio of 21:1, vice
    versa too).
    
    Initially, this was intended as a purely black and white theme but
    some contrast issues arose, such as the download badges (tertiary
    background, onTertiary text colour) having the same colour as unread
    badges (primary/onPrimary), or the step indicators (stops) not being
    visible on sliders (since they use the colours of the opposite state
    track (active region stops are the colour of the inactive region track
    and vice versa).
    
    To mitigate this, each variant (dark/light) of the theme has one
    additional grey mixed in for their tertiary and secondaryContainer
    colours each. For the dark variant, this is a #A0A0A0 background for
    #000000 text (8.03:1 contrast ratio) and for the light variant, it is
    a #505050 background for #FFFFFF text (8.06:1 contrast ratio).
    This results in distinct unread vs download badges and visible steps
    in the sliders.
    
    ---------
    
    Co-authored-by: Sunspark-007 <73711243+Sunspark-007@users.noreply.github.com>
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>
    Co-authored-by: MajorTanya <39014446+MajorTanya@users.noreply.github.com>

--- app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
+++ app/src/main/java/eu/kanade/domain/ui/model/AppTheme.kt
@@ -22,6 +22,7 @@ enum class AppTheme(val titleRes: StringResource?) {
     TIDAL_WAVE(MR.strings.theme_tidalwave),
     YINYANG(MR.strings.theme_yinyang),
     YOTSUBA(MR.strings.theme_yotsuba),
+    MONOCHROME(MR.strings.theme_monochrome),
 
     // Deprecated
     DARK_BLUE(null),
