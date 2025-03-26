51c903438fa0f648b0de0fc456212af9bf4be673
Secozzi <folke.steen85@gmail.com>
2025-03-26 11:22:14 +0100

    fix(player): Dismiss chapter sheet on chapter select

--- app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerViewModel.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerViewModel.kt
@@ -621,7 +621,7 @@ class PlayerViewModel @JvmOverloads constructor(
         _areControlsLocked.update { false }
     }
 
-    private fun dismissSheet() {
+    fun dismissSheet() {
         _dismissSheet.update { _ -> true }
     }
 
