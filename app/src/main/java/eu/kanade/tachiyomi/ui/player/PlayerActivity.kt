aniyomiorg/aniyomi

commit 10e28cc4092758cf38d27cc14aadf539698738f2

fix(player): Don't run unnecessary stuff when exiting the player (#1961)

app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerActivity.kt
@@ -269,17 +269,23 @@ class PlayerActivity : BaseActivity() {
     }
 
     override fun onDestroy() {
+        player.isExiting = true
+
         audioFocusRequest?.let {
             AudioManagerCompat.abandonAudioFocusRequest(audioManager, it)
         }
         audioFocusRequest = null
-        mediaSession?.release()
+
+        mediaSession?.let {
+            it.isActive = false
+            it.release()
+        }
+
         if (noisyReceiver.initialized) {
             unregisterReceiver(noisyReceiver)
             noisyReceiver.initialized = false
         }
 
-        player.isExiting = true
         MPVLib.removeLogObserver(playerObserver)
         MPVLib.removeObserver(playerObserver)
         player.destroy()
@@ -288,16 +294,22 @@ class PlayerActivity : BaseActivity() {
     }
 
     override fun onPause() {
-        if (!isInPictureInPictureMode) {
-            viewModel.pause()
-        }
         viewModel.saveCurrentEpisodeWatchingProgress()
+
+        if (isInPictureInPictureMode) {
+            super.onPause()
+            return
+        }
+
+        player.isExiting = true
+        if (isFinishing) {
+            MPVLib.command(arrayOf("stop"))
+        }
+
         super.onPause()
     }
 
     override fun onStop() {
-        viewModel.pause()
-        viewModel.saveCurrentEpisodeWatchingProgress()
         window.attributes.screenBrightness.let {
             if (playerPreferences.rememberPlayerBrightness().get() && it != -1f) {
                 playerPreferences.playerBrightnessValue().set(it)

commit ea749089b22b2a1c10b34acf0f997916c38bb49d

fix(player): Fix some issues caused by `10e28cc` (#1981)

app/src/main/java/eu/kanade/tachiyomi/ui/player/PlayerActivity.kt
@@ -314,6 +314,8 @@ class PlayerActivity : BaseActivity() {
         player.isExiting = true
         if (isFinishing) {
             MPVLib.command(arrayOf("stop"))
+        } else {
+            viewModel.pause()
         }
 
         super.onPause()
@@ -599,6 +601,12 @@ class PlayerActivity : BaseActivity() {
     }
 
     override fun onResume() {
+        if (!player.isExiting) {
+            super.onResume()
+            return
+        }
+
+        player.isExiting = false
         super.onResume()
 
         viewModel.currentVolume.update {