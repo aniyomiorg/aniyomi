281e7038e4a1c9ae6831a35102fd793fd9e3e7de
Secozzi <secozzi@proton.me>
2026-07-06 22:58:46 +0200

    Stop server when download is finished/stopped/errored

--- app/src/main/java/eu/kanade/tachiyomi/data/download/anime/AnimeDownloader.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/download/anime/AnimeDownloader.kt
@@ -408,6 +408,7 @@ class AnimeDownloader(
         video.status = Video.State.LOAD_VIDEO
 
         var progressJob: Job? = null
+        var isExternal = false
 
         // Get filename from download info
         val filename = DiskUtil.buildValidFilename(download.episode.name)
@@ -439,6 +440,7 @@ class AnimeDownloader(
 
                         downloadVideo(download, tmpDir, filename)
                     } else {
+                        isExternal = true
                         val betterFileName = DiskUtil.buildValidFilename(
                             "${download.anime.title} - ${download.episode.name}",
                         )
@@ -450,8 +452,12 @@ class AnimeDownloader(
             video.videoUrl = file.uri.path ?: ""
             download.progress = 100
             video.status = Video.State.READY
+            if (!isExternal) {
+                stopHttpServer(download)
+            }
             progressJob?.cancel()
         } catch (e: Exception) {
+            stopHttpServer(download)
             if (e is CancellationException) throw e
             video.status = Video.State.ERROR
             notifier.onError(e.message, download.episode.name, download.anime.title, download.anime.id)
@@ -630,6 +636,13 @@ class AnimeDownloader(
             }
             continuation.invokeOnCancellation { session.cancel() }
         }.output.toFloatOrNull()
+    }
+
+    private fun stopHttpServer(download: AnimeDownload) {
+        val server = download.source.server ?: return
+        if (server.isRunning()) {
+            server.stop()
+        }
     }
 
     /**
