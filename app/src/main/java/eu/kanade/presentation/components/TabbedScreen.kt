88b2ed11f54c822d44e3fd04bc9f5ff4ce3bfb21
lord-ne <34069872+lord-ne@users.noreply.github.com>
2026-07-30 00:27:52 +0300

    Try to make it not crash

--- app/src/main/java/eu/kanade/presentation/components/TabbedScreen.kt
+++ app/src/main/java/eu/kanade/presentation/components/TabbedScreen.kt
@@ -50,7 +50,7 @@ fun TabbedScreen(
     Scaffold(
         topBar = {
             if (titleRes != null) {
-                val tab = tabs[state.currentPage]
+                val tab = tabs.getOrElse(state.currentPage) { tabs.last() }
                 val searchEnabled = tab.searchEnabled
 
                 val actualQuery = when (state.currentPage % 2) {
@@ -113,7 +113,7 @@ fun TabbedScreen(
                 state = state,
                 verticalAlignment = Alignment.Top,
             ) { page ->
-                tabs[page].content(
+                tabs.getOrElse(state.currentPage) { tabs.last() }.content(
                     PaddingValues(bottom = contentPadding.calculateBottomPadding()),
                     snackbarHostState,
                 )
