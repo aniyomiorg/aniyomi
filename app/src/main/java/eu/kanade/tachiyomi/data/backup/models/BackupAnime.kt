1a0dde86549281f0f845c4ce466f1d103d410850
Secozzi <secozzi@proton.me>
2026-08-03 12:47:38 +0200

    Fix compatibility with older backups
    Co-authored-by: AntsyLich <59261191+AntsyLich@users.noreply.github.com>

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupAnime.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupAnime.kt
@@ -4,6 +4,7 @@ import eu.kanade.tachiyomi.animesource.model.AnimeUpdateStrategy
 import eu.kanade.tachiyomi.animesource.model.FetchType
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.protobuf.ProtoNumber
+import mihon.core.common.extensions.JsonObjectEmptyBytes
 import tachiyomi.data.MemoColumnAdapter
 import tachiyomi.domain.entries.anime.model.Anime
 
@@ -40,7 +41,7 @@ class BackupAnime(
     @ProtoNumber(106) var lastModifiedAt: Long = 0,
     @ProtoNumber(107) var favoriteModifiedAt: Long? = null,
     @ProtoNumber(109) var version: Long = 0,
-    @ProtoNumber(112) var memo: ByteArray = byteArrayOf(),
+    @ProtoNumber(112) var memo: ByteArray = JsonObjectEmptyBytes,
 
     // Aniyomi specific values
     @ProtoNumber(500) var backgroundUrl: String? = null,
--- app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupEpisode.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupEpisode.kt
@@ -3,6 +3,7 @@ package eu.kanade.tachiyomi.data.backup.models
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.protobuf.ProtoNumber
+import mihon.core.common.extensions.JsonObjectEmptyBytes
 import tachiyomi.data.MemoColumnAdapter
 import tachiyomi.domain.items.episode.model.Episode
 
@@ -27,14 +28,14 @@ class BackupEpisode(
     @ProtoNumber(10) var sourceOrder: Long = 0,
     @ProtoNumber(11) var lastModifiedAt: Long = 0,
     @ProtoNumber(12) var version: Long = 0,
-    @ProtoNumber(13) var memo: ByteArray = byteArrayOf(),
+    @ProtoNumber(13) var memo: ByteArray = JsonObjectEmptyBytes,
 
     // Aniyomi specific values
     @ProtoNumber(501) var fillermark: Boolean = false,
     @ProtoNumber(502) var summary: String? = null,
     @ProtoNumber(503) var previewUrl: String? = null,
 
-) {
+    ) {
     fun toEpisodeImpl(): Episode {
         return Episode.create().copy(
             url = this@BackupEpisode.url,
