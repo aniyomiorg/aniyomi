38e67d294c52979ecd6fe6a24c624973b8ec5d09
Secozzi <secozzi@proton.me>
2026-08-17 11:52:12 +0200

    Add `memo` to `Hoster` and `Video`

--- app/src/main/java/eu/kanade/presentation/entries/anime/EpisodeOptionsDialogScreen.kt
+++ app/src/main/java/eu/kanade/presentation/entries/anime/EpisodeOptionsDialogScreen.kt
@@ -380,13 +380,11 @@ class EpisodeOptionsDialogScreenModel(
                     hosterName = h.hosterName,
                     hosterUrl = h.hosterUrl,
                     videoList = (hosterStateList[index] as HosterState.Ready).videoList,
+                    internalData = h.internalData,
+                    memo = h.memo,
                 )
             } else {
-                Hoster(
-                    hosterName = h.hosterName,
-                    hosterUrl = h.hosterUrl,
-                    videoList = h.videoList,
-                )
+                h
             }
         }
     }
